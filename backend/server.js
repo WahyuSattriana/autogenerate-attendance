9dba19317a3939ad0691392a199b6e1baa3d07a5
faustroz <ferdysuper171@gmail.com>
2024-04-20 16:04:31 +0800

    feat: connect db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send({message: 'Hello World'});
-});
+app.use(express.json())
+
+const supabase = require('@supabase/supabase-js')
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
+
+app.get('/hadir', async (req, res) => {
+  const getData = await db.from("attendance").select()
+  res.json({ getData })
+})
+
+app.post('/', async (req, res) => {
+  const { nama, no_absen, kelas } = req.body
+  const insertData = await db.from("attendance").insert([{ nama, no_absen, kelas }])
+  res.json({ insertData })
+})
 
 app.listen(port, () => {
   console.log('Server running...')
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,25 @@
 const Page = async () => {
-  
-  const response =  await fetch("http://localhost:8080/");
-  const data = await response.json();
+  const axios = require('axios');
+  let response;
 
+  try {
+    response = await axios.get('http://localhost:8080/hadir');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
   return (
-    <>
-      <h1>{data.message}</h1>
-    </>
+    <div>
+      {response?.data.getData?.data?.map((data, index) => {
+        return (
+          <div key={index}>
+            <h1>{data.nama}</h1>
+            <h2>{data.no_absen}</h2>
+            <h3>{data.kelas}</h3>
+          </div>
+        )
+      })}
+    </div>
   );
 }
 
