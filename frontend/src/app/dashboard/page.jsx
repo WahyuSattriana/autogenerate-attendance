8f8e0bf3a66b875ae0af26e85e6b2cf4134b22b6
faustroz <ferdysuper171@gmail.com>
2024-05-06 19:30:02 +0800

    style: generate page styling

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -2,12 +2,18 @@ const Dashboard = () => {
   return (
     <div className="box-border flex flex-col align-center mt-5 max-w-screen-sm gap-5">
       <h1 className="text-center text-2xl font-bold">This is a Dashboard</h1>
-      <p>Here is your NISN</p>
-      <p>Here is your Name</p>
-      <p>Here is your kelas</p>
-      <p>Here is your Absen</p>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-lg font-semibold">NISN: 1234567890</p>
+          <p className="text-lg font-semibold">Name: John Doe</p>
+        </div>
+        <div>
+          <p className="text-lg font-semibold">Kelas: 10A</p>
+          <p className="text-lg font-semibold">Absen: 5</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Dashboard;
--- frontend/src/app/generate/page.jsx
+++ frontend/src/app/generate/page.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 const Page = async () => {
   const response = await axios.get('http://localhost:8080/generateRandomChar');
   return (
-    <div className="">
+    <div className="container mx-auto mt-4">
       <h1 className="text-center text-2xl font-bold">Generating QR Codes...</h1>
-      <Image src={response.data.QRData} width={300} height={300} alt={response.data.hasil}></Image>
-      <p>{response.data.randomChar}</p>
+      <div className="flex justify-center items-center mt-8">
+        <Image src={response.data.QRData} width={300} height={300} alt={response.data.hasil} />
+      </div>
+      <p className="text-center mt-4">{response.data.randomChar}</p>
     </div>
-  )
+  );
 }
 
 export default Page
