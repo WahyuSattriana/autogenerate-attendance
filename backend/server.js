0a4566931514c502b08bc0e2118e9c69845b98af
faustroz <ferdysuper171@gmail.com>
2024-05-11 23:20:34 +0800

    fix: backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const express = require('express');
 const QRCode = require('qrcode');
-const supabase = require('@supabase/supabase-js');
 
 const app = express();
 const port = 8080;
@@ -16,13 +15,12 @@ const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
 
 app.get('/generateRandomChar', async (req, res) => {
   const randomChar = crypto.randomBytes(10).toString('hex');
+  const getData = await db.from('profiles').insert();
   const QRData = await QRCode.toDataURL(randomChar);
   res.json({ randomChar: randomChar, QRData });
 });
 
 app.get('/hadir', async (req, res) => {
-  const getData = await db.from('attendance').select();
-  res.json({ getData });
 });
 
 app.listen(port, () => {
--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -11,7 +11,7 @@ export default async function LoginPage() {
         <h1 className="text-center text-2xl font-bold">Login / Sign Up</h1>
         <form className='flex flex-col gap-3'>
           <label htmlFor="nisn">NISN:</label>
-          <input id="nisn" name="nisn" type="text" required className='bg-slate-300 px-5 w-auto h-10 font-regular rounded-md border-2 border-slate-400' />
+          <input id="nisn" name="nisn" type="text" required className='bg-slate-300 px-5 w-auto h-10 font-regular rounded-md border-2 border-slate-400' maxLength={5} />
           <label htmlFor="email">Email:</label>
           <input id="email" name="email" type="email" required className='bg-slate-300 px-5 w-auto h-10 font-regular rounded-md border-2 border-slate-400' />
           <label htmlFor="password">Password:</label>
